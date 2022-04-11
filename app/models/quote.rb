407c6d02ab1b9c07b43029fbf54ea0d496422df1
Vladimir Zelenko <vzelenko@gmail.com>
2022-04-11 09:47:14 -0400

    Limit quote broadcasting scope to company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,5 +8,5 @@ class Quote < ApplicationRecord
   # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
   # after_destroy_commit -> { broadcast_remove_to 'quotes' }
-  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 end
