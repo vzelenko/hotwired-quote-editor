f7512a55996fb7d2bcaee061dc8f7db619809233
Vladimir Zelenko <vzelenko@gmail.com>
2022-04-11 10:14:17 -0400

    Disabled turbo drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "./controllers"
+
+Turbo.session.drive = false
