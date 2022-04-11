dddea29cf3ff4b958d32debadaad8f38172c2f76
Vladimir Zelenko <vzelenko@gmail.com>
2022-04-10 23:18:49 -0400

    Generate company model, migration, and test stubs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_022015) do
+ActiveRecord::Schema.define(version: 2022_04_11_031437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name"
