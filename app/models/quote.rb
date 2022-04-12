8c61e476a47639dce64ec9ed883131f1b5d9c324
Vladimir Zelenko <vzelenko@gmail.com>
2022-04-12 10:55:18 -0400

    Added quote totals

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   validates :name, presence: true
 
@@ -10,4 +11,9 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
   # after_destroy_commit -> { broadcast_remove_to 'quotes' }
   broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
+
 end
