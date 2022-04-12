04ed85f2a6725a5f5debddfff09a5341cd7fb8de
Vladimir Zelenko <vzelenko@gmail.com>
2022-04-12 10:27:26 -0400

    Added Line Items to dates

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -1,11 +1,12 @@
 class LineItemDate < ApplicationRecord
   belongs_to :quote
+  has_many :line_items, dependent: :destroy
 
-  validates :date, presence: true, uniqueness: {scope: :quote_id}
+  validates :date, presence: true, uniqueness: { scope: :quote_id }
 
   scope :ordered, -> { order(date: :asc) }
 
   def previous_date
-    quote.line_item_dates.ordered.where("date < ?", date).last
+    quote.line_item_dates.ordered.where('date < ?', date).last
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :quotes do
-    resources :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
